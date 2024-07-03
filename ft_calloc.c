f0a80bc526576a1ff6bba883c7299e7cf74dee47
arnoop88 <arnaupodaderav@gmail.com>
2024-07-03 19:58:17 +0200

    all

---
+++ ft_calloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apodader <apodader@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 11:45:52 by apodader          #+#    #+#             */
+/*   Updated: 2023/01/25 14:29:35 by apodader         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+void	*ft_calloc(size_t n, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(n * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, n * size);
+	return (ptr);
+}
